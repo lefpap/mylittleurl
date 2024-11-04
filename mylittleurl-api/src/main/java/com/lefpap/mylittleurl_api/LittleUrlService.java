53f6991a8c787806424073bdfe92173d3a6168aa
lefpap <lolopap21@gmail.com>
2024-11-04 22:30:59 +0200

    Added short code configuration and generation strategy

--- mylittleurl-api/src/main/java/com/lefpap/mylittleurl_api/LittleUrlService.java
+++ mylittleurl-api/src/main/java/com/lefpap/mylittleurl_api/LittleUrlService.java
@@ -1,12 +1,15 @@
 package com.lefpap.mylittleurl_api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class LittleUrlService {
 
@@ -14,38 +17,49 @@ public class LittleUrlService {
     private final LittleUrlCodeGen urlCodeGen;
 
     public RedirectView resolveUrlCode(String code) {
+        log.info("Resolving URL for code: {}", code);
         String fullUrl = urlRepository.findByCode(code)
             .map(this::updateUrlClickCount)
             .map(LittleUrl::url)
             .orElseThrow();
 
-        return new RedirectView(fullUrl);
+        log.info("Redirecting to URL: {} for code: {}", fullUrl, code);
+        return new RedirectView(fullUrl, false, false);
     }
 
     private LittleUrl updateUrlClickCount(LittleUrl littleUrl) {
+        long updatedClickCount = littleUrl.clickCount() + 1;
+
+        log.debug("Updating click count for code: {} to {}", littleUrl.code(), updatedClickCount);
         return urlRepository.save(littleUrl.toBuilder()
-            .clickCount(littleUrl.clickCount() + 1)
+            .clickCount(updatedClickCount)
             .build()
         );
     }
 
     public LittleUrl findByCode(String code) {
+        log.info("Finding URL for code: {}", code);
         return urlRepository.findByCode(code)
                 .orElseThrow();
     }
 
+    @Transactional
     public LittleUrl create(LittleUrlRequestDTO request) {
-        return urlRepository.findByUrl(request.url())
-                .orElse(buildNewLittleUrl(request));
-    }
 
-    private LittleUrl buildNewLittleUrl(LittleUrlRequestDTO request) {
-        LittleUrl.LittleUrlBuilder builder = LittleUrl.builder();
-        builder.code(urlCodeGen.generateUniqueCode());
-        builder.url(request.url());
+        log.info("Creating a new shortened URL for: {}", request.url());
+        String code = urlCodeGen.generateUniqueCode();
+
+        // Build the LittleUrl entity
+        LittleUrl.LittleUrlBuilder builder = LittleUrl.builder()
+                .code(code)
+                .url(request.url());
+
         Optional.ofNullable(request.expiresAt()).ifPresent(builder::expiresAt);
 
-        return urlRepository.save(builder.build());
+        LittleUrl littleUrl = urlRepository.save(builder.build());
+        log.info("Successfully saved shortened URL with code: {}", littleUrl.code());
+
+        return littleUrl;
     }
 
 }
